77115001775c4e553ec0c23b50254eed8a60c8d8
olenikov <a.olenikov@me.com>
2022-12-10 20:27:02 +0300

    4 задача, не понятно как выводить 0 или 1 месяц

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         int total = 0;
         int depoMounth = 15000;
         int mounthCount = 1;
-        while (total < 2_459_000 ){
-            total = total + depoMounth + (total/100);
+        while (total < 2_459_000) {
+            total = total + depoMounth + (total / 100);
             mounthCount = mounthCount + 1;
             System.out.println("Месяц " + mounthCount + " сумма накоплений равна " + total + " рублей");
         }
@@ -15,24 +15,34 @@ public class Main {
             System.out.print(number + " ");
         }
         System.out.println();
-        for (;number > 0; number-- ){
+        for (; number > 0; number--) {
             System.out.print(number + " ");
         }
 
-      System.out.println();
-      int population = 12_000_000 ;
-      int born = population/1000*17;
-      int death = population/1000*8;
-      int start = 0;
-      while ( start<10){
-          population = population + (born - death) * 12;
-          start = start + 1;
-          System.out.println("Год "+ start + " численность населения составляет " + population);
+        System.out.println();
+        int population = 12_000_000;
+        int born = population / 1000 * 17;
+        int death = population / 1000 * 8;
+        int start = 0;
+        while (start < 10) {
+            population = population + (born - death) * 12;
+            start = start + 1;
+            System.out.println("Год " + start + " численность населения составляет " + population);
 
-      }
+        }
+
+
+        double tDepo = 15_000;
+        int mCount = 1;
+        while (tDepo < 12_000_000) {
+            tDepo = tDepo + (tDepo * 0.07) + 15_000;
+            mCount = mCount + 1;
+            System.out.println("Месяц " + mCount + " Сумма накоплений " + tDepo);
+
+
+        }
+
+
     }
-
-
-
-    }
+}
 
